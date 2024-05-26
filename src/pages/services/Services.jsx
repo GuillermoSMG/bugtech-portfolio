4878f6cf46d9bad067ed17e8ed003772ad59417e
Guillermo San Martín <guille.seba0802@gmail.com>
2024-05-26 20:27:53 -0300

    services

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import { Service } from './components/ServiceContainer';
 import { Values } from './components/Values';
 import { Advantages } from './components/Advantages';
+import { CHARACTERISTICS, SERVICES } from './const/services';
 
 export const Services = () => {
 	return (
 		<section>
-			<Service />
+			<Service services={SERVICES} />
 			<Values />
 			<Advantages />
+			<Service services={CHARACTERISTICS} />
 		</section>
 	);
 };
