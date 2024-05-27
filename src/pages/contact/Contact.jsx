a46ef4561e330c9c31e82986aac0e19b8688098c
Guillermo San Martín <guille.seba0802@gmail.com>
2024-05-27 01:13:01 -0300

    contact

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -4,10 +4,7 @@ import { CONTACT } from './const/contact';
 
 export const Contact = () => {
 	return (
-		<section
-			className='text-center lg:pt-10 lg:mt-10 lg:pb-20 md:pt-8 md:mt-8 md:pb-16 px-4 pt-6 mt-6 pb-12 md:px-6'
-			id='contact'
-		>
+		<section className='text-center  px-4 pt-24 pb-12 md:px-6' id='contact'>
 			<h2 className='dark:text-white text-linkText text-4xl md:text-6xl font-semibold tracking-tighter md:tracking-normal md:mb-2'>
 				¡Contactanos!
 			</h2>
